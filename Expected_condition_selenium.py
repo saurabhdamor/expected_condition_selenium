bcca792dfe96173f12d1ac69b9ba454932288957
Saurabh Damor <79511798+saurabhdamor@users.noreply.github.com>
2021-06-21 11:31:30 +0530

    Add files via upload

---
+++ Expected_condition_selenium.py
@@ -0,0 +1,28 @@
+#  From selenium library, imported webdriver
+from selenium import webdriver
+
+# Imported By
+from selenium.webdriver.common.by import By
+
+# Imported WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Imported Expected Condition as EC
+from selenium.webdriver.support import expected_conditions as EC
+
+# Here we set 'gecodriver.exe' path in '   '
+driver = webdriver.Firefox(executable_path=r'C:\Users\saurabh\Documents\gecodriver\geckodriver.exe')
+
+# Here we set web-site url in "  "
+driver.get("http://somedomain/url_that_delays_loading")
+
+# Here you can set your required 'expected condition ' & also your ' Elements' By.(XPATH, ID, TAGNAME, etc...)
+# Also you can change waiting time period : (driver,'Your required wait time' )
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+
+# Here we close our browser using .quit() method
+    driver.quit()
