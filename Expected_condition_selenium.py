191e88cc9bc56de60bd8baedf3d38b595500b80e
Saurabh Damor <79511798+saurabhdamor@users.noreply.github.com>
2021-06-21 11:40:53 +0530

    Update Expected_condition_selenium.py

--- Expected_condition_selenium.py
+++ Expected_condition_selenium.py
@@ -14,13 +14,13 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Firefox(executable_path=r'C:\Users\saurabh\Documents\gecodriver\geckodriver.exe')
 
 # Here we set web-site url in "  "
-driver.get("http://somedomain/url_that_delays_loading")
+driver.get("https://www.google.com/")
 
 # Here you can set your required 'expected condition ' & also your ' Elements' By.(XPATH, ID, TAGNAME, etc...)
 # Also you can change waiting time period : (driver,'Your required wait time' )
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[2]"))
     )
 finally:
 
